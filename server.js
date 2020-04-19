db7ad55429cb2649b5633bd8d48a99c2f9d00646
Rohit Jain <rohitjain991100@gmail.com>
2020-04-19 11:47:54 +0530

    xxx

--- server.js
+++ server.js
@@ -26,7 +26,7 @@ app.use(bodyParser.json());
 app.use(cors());
 
 app.get('/',(req, res)=>{
- res.send("it is working");  
+ res.send("it is working")
 })
 
 app.post('/signin',(req,res)=>{signin.handleSignIn(req,res,db,passwordHash)})
